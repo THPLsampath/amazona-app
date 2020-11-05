099182d025a91beb9866293f06c570a35662c539
sampath <sampaththpl@gmail.com>
2020-11-05 19:07:05 +0530

    save the code in github

---
+++ frontendcode/src/data.js
@@ -0,0 +1,70 @@
+export const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'nick slim shart',
+            category: 'shart',
+            image: '/images//product-1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            descripation: 'high qulity produvct'
+        },
+        {
+            _id: '2',
+            name: 'Adidas fit shart',
+            category: 'shart',
+            image: '/images//product-2.jpg',
+            price: 130,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 40,
+            descripation: 'high qulity produvct'
+        },
+        {
+            _id: '3',
+            name: 'Locost free shart',
+            category: 'shart',
+            image: '/images//product-3.jpg',
+            price: 120,
+            brand: 'Locost',
+            rating: 4.5,
+            numReviews: 10,
+            descripation: 'high qulity produvct'
+        },
+        {
+            _id: '4',
+            name: 'nick slim Pants',
+            category: 'Pants',
+            image: '/images//product-4.jpg',
+            price: 78,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            descripation: 'high qulity produvct'
+        },
+        {
+            _id: '5',
+            name: 'puma slim pant',
+            category: 'pant',
+            image: '/images//product-5.jpg',
+            price: 80,
+            brand: 'puma',
+            rating: 4.5,
+            numReviews: 40,
+            descripation: 'high qulity produvct'
+        },
+        {
+            _id: '6',
+            name: 'Adidas slim pant',
+            category: 'pant',
+            image: '/images//product-6.jpg',
+            price: 56,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 10,
+            descripation: 'high qulity produvct'
+        },
+    ]
+}
